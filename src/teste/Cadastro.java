8065091501c29a3bd88418d1c41fab2fe7b3a502
jnbrecife <jnbrecife@FJN005-L105-019.serlab.intra>
2016-09-08 08:23:48 -0300

    Subindo projeto

---
+++ src/teste/Cadastro.java
@@ -0,0 +1,50 @@
+
+package teste;
+
+
+public class Cadastro {
+    
+    private String nome;
+    private double cpf;
+    private String endereco;
+    private double salario;
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    public double getCpf() {
+        return cpf;
+    }
+
+   
+    public void setCpf(double cpf) {
+        this.cpf = cpf;
+    }
+
+    
+    public String getEndereco() {
+        return endereco;
+    }
+
+    
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+}
